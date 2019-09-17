fa764078f5d03ea982c588c0e92950a201dabe41
Nicolás Alliaume <nicolasalliaume@gmail.com>
2019-09-17 14:57:07 -0300

    Initial commit

--- index.js
+++ index.js
@@ -1,40 +1,29 @@
-module.exports = function( argv ) {
-	const result = { };
-	let currentKey = null;
+const readline = require( 'readline-sync' );
 
-	for ( var i = 0; i < argv.length; i++ ) {
-		let arg = argv[ i ];
+/**
+ * Prompts the user for an input. May provide options
+ * as second parameter, and a default option. If no
+ * default is provided, first option will be used.
+ * 
+ * @param  {String} message       
+ * @param  {Array}  options       
+ * @param  {Array}  defaultOption 
+ * @return {String}               
+ */
+module.exports = function( 
+	message, 
+	options = ['Y/y', 'N/n'], 
+	defaultOption = 'y' 
+) {
+	const optionsStr = options && options.length > 0 
+		? `(${ options.join( ', ' ) }) [${ defaultOption || options[0] }]` 
+		: '';
 		
-		// Found new flag / param
-		if ( arg.startsWith( '-' ) ) {
-			// if no value for prev arg, then it's
-			// a flag. Close prev and set up new flag / param
-			if ( currentKey ) {
-				result[ currentKey ] = true;
-				currentKey = null;
-			}
-			arg = arg.replace(/-/g, '');
-			result[ arg ] = true;
-			currentKey = arg;
-		}
-		// Working on a key already. Get value and
-		// finish this key.
-		else if ( currentKey ) {
-			result[ currentKey ] = arg;
-			currentKey = null;
-		}
-		// No prev param to complete, and this is not
-		// a flag. So it's either the action we wanna do
-		// or some non-flagged param. Using special key "__"
-		// for this.
-		else {
-			if ( result[ '__' ] == undefined ) result[ '__' ] = [];
-			result[ '__' ].push( arg );
-			// make sure we reset the current key so we don't assign
-			// other inline parameters (non-flagged) to prev flags
-			currentKey = null;
-		}
+	const response = readline.question( `${ message }${ optionsStr }: ` );
+	
+	if ( ! response ) {
+		return defaultOption;
 	}
-
-	return result;
+	
+	return response;
 }
